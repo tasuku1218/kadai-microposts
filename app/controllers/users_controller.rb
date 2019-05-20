d183b957b23c60cda2ac56ef921663d95619f4bd
t.c <s03210462000@yahoo.co.jp>
2019-05-20 11:40:58 +0000

    faborite

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,18 @@
 class UsersController < ApplicationController
-  before_action :require_user_logged_in, only: [:index, :show, :followings, :followers]
-  
+  before_action :require_user_logged_in, only: [:index, :show, :followings, :followers, :likes,]
   def index
     @users = User.all.page(params[:page])
   end
-
   def show
     @user = User.find(params[:id])
     @microposts = @user.microposts.order('created_at DESC').page(params[:page])
     counts(@user)
   end
-
   def new
     @user = User.new
   end
-
   def create
     @user = User.new(user_params)
-
     if @user.save
       flash[:success] = 'ユーザを登録しました。'
       redirect_to @user
@@ -26,21 +21,25 @@ class UsersController < ApplicationController
       render :new
     end
   end
-  
   def followings
     @user = User.find(params[:id])
     @followings = @user.followings.page(params[:page])
     counts(@user)
   end
-  
   def followers
     @user = User.find(params[:id])
     @followers = @user.followers.page(params[:page])
     counts(@user)
   end
+  def likes
+    @user = User.find(params[:id])
+    @likes = @user.likes.page(params[:page])
+    counts(@user)
 
+  end
+
+  
   private
-
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,9 @@ class User < ApplicationRecord
   has_many :reverses_of_relationship, class_name: 'Relationship', foreign_key: 'follow_id'
   has_many :followers, through: :reverses_of_relationship, source: :user
   
+  has_many :favorites
+  has_many :likes, through: :favorites, source: :micropost
+  
   def follow(other_user)
     unless self == other_user
       self.relationships.find_or_create_by(follow_id: other_user.id)
@@ -26,10 +29,24 @@ class User < ApplicationRecord
   def following?(other_user)
     self.followings.include?(other_user)
   end
+
+  def like(micropost)
+    unless self == micropost
+      self.favorites.find_or_create_by(micropost_id: micropost.id)
+    end
+  end
+
+  def unlike(micropost)
+    favorite = self.favorites.find_by(micropost_id: micropost.id)
+    favorite.destroy if favorite
+  end
+
+  def likes?(micropost)
+    self.likes.include?(micropost)
+  end
   
   def feed_microposts
     Micropost.where(user_id: self.following_ids + [self.id])
   end
   
 end
-
